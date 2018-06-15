52c99a7f195491eab517a7703403e6f2ea43f67d
Mahmoud Khalifa <mahmoudkhalifa@hotmail.co.uk>
2018-06-15 14:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
- function takeANumber(line, name) {
-   line.push(name)
-return `Welcome, ${name}. You are number ${line.length} in line.`
+ function takeANumber(line number,) {
+   line.push(number)
+return `Welcome, ${number}.  ${line.length} in line.`
 }
 
 function nowServing(line) {
